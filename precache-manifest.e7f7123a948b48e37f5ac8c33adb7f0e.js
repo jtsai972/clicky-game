805cf6977c21299c020f433abcd72664dba863e3
J Tsai <jtsai972@gmail.com>
2019-11-13 23:43:17 -0600

    Updates

---
+++ precache-manifest.e7f7123a948b48e37f5ac8c33adb7f0e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1026b021b78a19c1d2a14d7832f720c",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "aae8133101588403a0a0",
+    "url": "/clicky-game/static/js/2.ed98f89d.chunk.js"
+  },
+  {
+    "revision": "37083b5811eec893e491",
+    "url": "/clicky-game/static/js/main.8c804278.chunk.js"
+  },
+  {
+    "revision": "72844d01c986203ad4dc",
+    "url": "/clicky-game/static/js/runtime-main.66ab81c2.js"
+  }
+]);
